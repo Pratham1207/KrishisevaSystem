5049202a1ccf6bc7dc5f9cbbd30273de1987ba18
Yash19Yp <yashpancholi001@gmail.com>
2025-02-12 12:55:54 -0500

    User Authentication feature: Register and Login

---
+++ Backend/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  role: {
+    type: String,
+    enum: ["farmer", "admin", "csowner"], // csowner - cold storage owner
+    default: "farmer",
+  },
+});
+
+// Hash password before saving user
+UserSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
